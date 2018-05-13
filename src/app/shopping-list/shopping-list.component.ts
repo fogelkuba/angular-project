02889b504e3701027c7cda814d9fa2ae91b9b283
fogelkuba <fogelkuba@gmail.com>
2018-05-13 18:32:24 +0200

    fix

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,7 +7,7 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredientes: Ingridient[] = [
+  ingredientes: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ];
