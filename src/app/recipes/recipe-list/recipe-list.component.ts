cba2b0b34c9d7be32aea62ff6698d498f78e7844
fogelkuba <fogelkuba@gmail.com>
2018-07-07 15:56:08 +0200

    fix

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,16 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'Pizza',
-      'campbells vegetarian pizza recipe',
-      'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg'),
-    new Recipe(
-      'Stir-Fried Lo Mein Noodles',
-      'Stir-Fried Lo Mein Noodles With Pork and Vegetables Recipe',
-      'https://www.seriouseats.com/recipes/images/2017/03/Stir_Fried_Lo_Mein_20170315_3-edit-1500x1125.jpg')
-  ];
+
 
   constructor() { }
 
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,18 @@
-export class RecipeService{
+import {Recipe} from "./recipe.model";
 
+export class RecipeService{
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Pizza',
+      'campbells vegetarian pizza recipe',
+      'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg'),
+    new Recipe(
+      'Stir-Fried Lo Mein Noodles',
+      'Stir-Fried Lo Mein Noodles With Pork and Vegetables Recipe',
+      'https://www.seriouseats.com/recipes/images/2017/03/Stir_Fried_Lo_Mein_20170315_3-edit-1500x1125.jpg')
+  ];
+
+  getRecipes(){
+    return this.recipes.slice();
+  }
 }
