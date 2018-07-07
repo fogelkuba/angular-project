c25d6e74ee8aaa72292317d6dee002db7e9b8be3
fogelkuba <fogelkuba@gmail.com>
2018-07-07 17:54:43 +0200

    fix

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService{
 
@@ -9,11 +10,20 @@ export class RecipeService{
     new Recipe(
       'Pizza',
       'campbells vegetarian pizza recipe',
-      'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg'),
+      'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Ananas', 10)
+      ]),
     new Recipe(
       'Stir-Fried Lo Mein Noodles',
       'Stir-Fried Lo Mein Noodles With Pork and Vegetables Recipe',
-      'https://www.seriouseats.com/recipes/images/2017/03/Stir_Fried_Lo_Mein_20170315_3-edit-1500x1125.jpg')
+      'https://www.seriouseats.com/recipes/images/2017/03/Stir_Fried_Lo_Mein_20170315_3-edit-1500x1125.jpg',
+      [
+        new Ingredient('Chicken', 1),
+        new Ingredient('Noodle', 10),
+        new Ingredient('Vegetables', 5),
+      ]),
   ];
 
   getRecipes(){
