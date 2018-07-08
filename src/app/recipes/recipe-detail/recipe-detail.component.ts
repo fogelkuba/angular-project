7cccb69db8273fba1f1e9dd00c1cb0bfab356c01
fogelkuba <fogelkuba@gmail.com>
2018-07-08 18:59:43 +0200

    fix

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+
 import {Recipe} from "../recipe.model";
 import {RecipeService} from "../recipe.service";
-import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -16,14 +17,13 @@ export class RecipeDetailComponent implements OnInit {
              private route: ActivatedRoute){ }
 
   ngOnInit() {
-    // const id = this.route.snapshot.params['id'];
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.id = +params['id'];
-        this.recipeService.getRecipe(this.id);
-      }
+    this.route.params
+        .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.id);
+        }
     )
-    // this.route
   }
 
   onAddToShoppingList(){
