6809d193b4748e9f8cc23bf167fd2e40b3591d7e
fogelkuba <fogelkuba@gmail.com>
2018-08-07 21:30:31 +0200

    fix

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,9 +2,11 @@ import {Recipe} from "./recipe.model";
 import {Injectable} from "@angular/core";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
+import {Subject} from "rxjs/index";
 
 @Injectable()
 export class RecipeService{
+  recipesChanged = new Subject<Recipe[]>();
 
   // private recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
@@ -55,10 +57,12 @@ export class RecipeService{
   }
 
   addRecipe(recipe: Recipe) {
-
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   updateRecipe(index: number, newRecipe: Recipe){
     this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
