031385ed24661a7c1422883e438b1c049d687911
fogelkuba <fogelkuba@gmail.com>
2018-08-13 20:28:30 +0200

    fix

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,4 +1,14 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {RecipeService} from "../recipes/recipe.service";
 
+@Injectable()
 export class DataStorageService {
+    constructor (private http: Http, private recipeService: RecipeService) {}
 
+    private url = 'https://ng-recipe-book-f046d.firebaseio.com/';
+
+    storeRecipes() {
+        this.http.put(url + 'recipes/json');
+    }
 }
