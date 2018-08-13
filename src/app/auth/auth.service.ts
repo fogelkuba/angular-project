0d1d986486cf22da82f02136360b36f000f9e39c
fogelkuba <fogelkuba@gmail.com>
2018-08-13 22:48:47 +0200

    fix

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,15 @@
 import {Injectable} from "@angular/core";
+import * as firebase from 'firebase';
 
-@Injectable()
+// @Injectable()
 
 export class AuthService {
     signupUser(email: string, password: string) {
-
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(
+            err => {
+                console.log(err);
+            }
+        )
     }
 }
