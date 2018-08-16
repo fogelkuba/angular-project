c9f99c48bd09e750ea957bf3086fd91d3b90545d
fogelkuba <fogelkuba@gmail.com>
2018-08-16 23:15:12 +0200

    fix

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -1,12 +1,24 @@
 import {NgModule} from "@angular/core";
+import {Routes} from "@angular/router";
+import {AuthGuard} from "../auth/auth-guard.service";
+
+import {RecipesComponent} from "./recipes.component";
+import {RecipeStartComponent} from "./recipe-start/recipe-start.component";
+import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
+import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
 
 const recipesRoutes: Routes = [
-
+    {
+        path: 'recipes', component: RecipesComponent, children: [
+            {path: '', component: RecipeStartComponent},
+            {path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard]},
+            {path: ':id', component: RecipeDetailComponent},
+            {path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard]},
+        ]
+    },
 ];
 
-@NgModule({
-
-})
+@NgModule({})
 export class RecipesRoutingModule {
-    
+
 }
